4a80048952a7d7e7a04096bac1af83a75b113f62
Yuki (//w//) <indraniko20@gmail.com>
2022-11-21 20:46:21 +0700

    tara

--- main.py
+++ main.py
@@ -34,12 +34,10 @@ def feature():
     session['f1'] = request.form.get("feature1")
     session['f2'] = request.form.get("feature2")
 
-    f1 = request.form.get("feature1")
-    f2 = request.form.get("feature2")
+    f1 = int(request.form.get("feature1"))
+    f2 = int(request.form.get("feature2"))
 
-    if ((not f1=='') and (not f2=='')):
-        f1 = int(f1)
-        f2 = int(f2)
+    if ((not f1==-1) and (not f2==-1)):
         elbowVis(f1,f2)
         a = feaSel(f1)
         b = feaSel(f2)
